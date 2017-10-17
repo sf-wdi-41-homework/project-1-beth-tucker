c5df79769e29424bef72018f82f331190b9bfa93
brwitten <bethrwitten@gmail.com>
2017-10-17 12:13:44 -0700

    modals for new to do and new list are working; CSS formatting is off

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -35,27 +35,27 @@ $(document).ready(function() {
       }
   }
 
-  // // New List Modal
-  // // Get the modal
-  // var modal = document.getElementById('myModal');
-  // // Get the button that opens the modal
-  // var btn = document.getElementById('toDoBtn');
-  // // Get the <span> element that closes the modal
-  // var span = document.getElementsByClassName("close")[0];
-  // // When the user clicks on the button, open the modal
-  // btn.onclick = function() {
-  //     modal.style.display = "block";
-  // }
-  // // When the user clicks on <span> (x), close the modal
-  // span.onclick = function() {
-  //     modal.style.display = "none";
-  // }
-  // // When the user clicks anywhere outside of the modal, close it
-  // window.onclick = function(event) {
-  //     if (event.target == modal) {
-  //         modal.style.display = "none";
-  //     }
-  // }
+  // New List Modal
+  // Get the modal
+  var newListModal = document.getElementById('listModal');
+  // Get the button that opens the modal
+  var newListBtn = document.getElementById('newListBtn');
+  // Get the <span> element that closes the modal
+  var newListSpan = document.getElementById('newListClose')[0];
+  // When the user clicks on the button, open the modal
+  newListBtn.onclick = function() {
+      newListModal.style.display = "block";
+  }
+  // When the user clicks on <span> (x), close the modal
+  newListClose.onclick = function() {
+      newListModal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == newListModal) {
+          newListModal.style.display = "none";
+      }
+  }
 
 
 });
