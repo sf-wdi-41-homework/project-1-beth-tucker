c0c7cb8b38d6d9862aef8d79ae97145bf5e225d4
tuckbolding <tuckbolding@gmail.com>
2017-10-17 11:26:28 -0700

    required all models in index

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 // require mongoose and connect to database
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/jibe_test');
+mongoose.connect('mongodb://localhost/jibe');
 
 var User = require('./user');
 var Todo = require('./todo');
-
+var List = require('./list');
 
 module.exports = {
   User: User,
   Todo: Todo,
-
+  List: List
 };
